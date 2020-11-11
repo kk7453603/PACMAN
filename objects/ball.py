3913dcc3b9cb37c04b38bbc84a32d8e24280abc9
Ivanchenko Anton <anton.333.play@gmail.com>
2020-11-11 12:59:17 +0300

    Ivanchenko: 	[CREATE] new object character.py 	[ADD] character.py to object/__init__.py 	[EDIT] ball.py to inheritance from character.py 	[DELETE] default image "images/basketball.png" from image.py and 	ball.py

--- objects/ball.py
+++ objects/ball.py
@@ -1,26 +1,15 @@
 import pygame
 
-from misc import get_nonzero_random_value
-from objects.image import ImageObject
+from objects.character import CharacterObject
 
 
-class BallObject(ImageObject):
+class BallObject(CharacterObject):
     filename = 'images/basketball.png'
     image = pygame.image.load(filename)
-    MAX_SPEED = 2
 
     def __init__(self, game, x: int = 0, y: int = 0, speed: int = None) -> None:
-        super().__init__(game)
-        self.rect.x = x
-        self.rect.y = y
+        super().__init__(game, x, y, speed)
         self.radius = self.rect.width // 2
-        self.speed = speed if speed else [
-            get_nonzero_random_value(BallObject.MAX_SPEED),
-            get_nonzero_random_value(BallObject.MAX_SPEED)
-        ]
-
-    def collides_with(self, other) -> bool:
-        return pygame.sprite.collide_circle(self, other)
 
     def bounce(self, other) -> None:
         self.speed, other.speed = other.speed, self.speed
--- objects/character.py
+++ objects/character.py
@@ -11,7 +11,6 @@ class CharacterObject(ImageObject):
         super().__init__(game)
         self.rect.x = x
         self.rect.y = y
-        self.radius = self.rect.width // 2
         self.speed = speed if speed else [
             get_nonzero_random_value(CharacterObject.MAX_SPEED),
             get_nonzero_random_value(CharacterObject.MAX_SPEED)
@@ -19,29 +18,3 @@ class CharacterObject(ImageObject):
 
     def collides_with(self, other) -> bool:
         return pygame.sprite.collide_circle(self, other)
-
-    def bounce(self, other) -> None:
-        self.speed, other.speed = other.speed, self.speed
-
-    def vertical_edge_collision(self) -> bool:
-        return self.rect.right >= self.game.WIDTH or self.rect.left <= 0
-
-    def horisontal_edge_collision(self) -> bool:
-        return self.rect.bottom >= self.game.HEIGHT or self.rect.top <= 0
-
-    def edge_collision(self) -> bool:
-        return self.horisontal_edge_collision() or self.vertical_edge_collision()
-
-    def check_borders(self) -> None:
-        if self.vertical_edge_collision():
-            self.speed[0] *= -1
-        if self.horisontal_edge_collision():
-            self.speed[1] *= -1
-
-    def step(self) -> None:
-        self.rect.x += self.speed[0]
-        self.rect.y += self.speed[1]
-
-    def process_logic(self) -> None:
-        self.check_borders()
-        self.step()
