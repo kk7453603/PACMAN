0aad049a03cd926796ba88e6b45a2b5a124d3a78
Vlada <vlada.kocherova@gmail.com>
2020-11-13 23:01:12 +0300

    Kocherova: seed class

--- objects/seed.py
+++ objects/seed.py
@@ -4,13 +4,12 @@ class SeedObject:
     
     def __init__(self, game, type):
         super().__init__(game)
+        self.game = game
         self.type = type
         if type = 0:
-            filename = 'point.png'
+            filename = 'images/map/point.png'
         else:
-            filename = 'tablet.png'
+            filename = 'images/map/tablet.png'
         self.image = pygame.image.load(filename)
         self.rect = self.image.get_rect()
 
-    def draw_seed(self):
-        screen.blit(self.image, self.rect)
