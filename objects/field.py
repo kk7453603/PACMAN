eb020951a4c02928c16cc8a9c4ea79281044fece
Ivanchenko Anton <anton.333.play@gmail.com>
2020-11-21 19:54:52 +0300

    Ivanchenko: 	[ADD] functions get_pathways and get_walls in objects/field.py 	[EDIT] filed default.py (deleted all cell with value '1')

--- objects/field.py
+++ objects/field.py
@@ -8,15 +8,34 @@ from constants import Color
 class FieldObject(DrawableObject):
     field = fieldArr
 
-    def __init__(self, game, x, y):
+    def __init__(self, game, x: int = 0, y: int = 0, cell_width: int = 10, cell_height: int = 10) -> None:
         super().__init__(game)
-        self.x = x
-        self.y = y
+        self.cell_width = cell_width
+        self.cell_height = cell_height
+        self.rect = pygame.rect.Rect(x, y, self.cell_width * len(self.field[0]), self.cell_height * len(self.field))
+
+    def get_pathways(self) -> [[pygame.rect.Rect()]]:
+        pathways = []
+        for (i, row) in enumerate(self.field):
+            for (j, cell) in enumerate(row):
+                if cell == '1':
+                    x = self.rect.x + j * self.cell_width
+                    y = self.rect.y + x * self.cell_height
+                    pathways.append(pygame.rect.Rect(x, y, self.cell_width, self.cell_height))
+        return pathways
+
+    def get_walls(self) -> [[pygame.rect.Rect()]]:
+        walls = []
+        for (i, row) in enumerate(self.field):
+            for (j, cell) in enumerate(row):
+                if cell == '2':
+                    x = self.rect.x + j * self.cell_width
+                    y = self.rect.y + x * self.cell_height
+                    walls.append(pygame.rect.Rect(x, y, self.cell_width, self.cell_height))
+        return walls
 
     def process_draw(self) -> None:
-        y = self.y
         for (i, row) in enumerate(self.field):
-            x = self.x
             for (j, cell) in enumerate(row):
                 if cell == 0:
                     color = Color.WHITE
@@ -24,7 +43,6 @@ class FieldObject(DrawableObject):
                     color = Color.GREEN
                 elif cell == 2:
                     color = Color.BLUE
-                pygame.draw.rect(self.game.screen, color, (x, y, 10, 10))
-                print("[{}][{}] cell == {} color == {}".format(i, j, cell, color))
-                x += 10
-            y += 10
+                x = self.rect.x + self.cell_width * j
+                y = self.rect.y + self.cell_height * i
+                pygame.draw.rect(self.game.screen, color, (x, y, self.cell_width, self.cell_height))
