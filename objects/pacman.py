bbe6fec0f3777a9a42fa6625d3112afc8083188c
Berezin Nikolai <yorkowolf@gmail.com>
2020-11-27 17:01:05 +0300

    Optimized pacman class

--- objects/pacman.py
+++ objects/pacman.py
@@ -9,7 +9,7 @@ class PacmanObject(CharacterObject):
     image_copy = pygame.transform.scale(image_copy, (150, 150))
     image = image_copy
 
-    def __init__(self, game, x: int = 0, y: int = 0 ) -> None:
+    def __init__(self, game, x: int = 0, y: int = 0) -> None:
         super().__init__(game, x, y)
         self.rotated_image = self.image_copy
         self.direction = "NONE"
@@ -18,44 +18,43 @@ class PacmanObject(CharacterObject):
         self.speed[1] = 0
         self.radius = self.rect.width // 2
 
-    def get_direction(self) -> None:
-        for event in pygame.event.get():
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_w or event.key == pygame.K_UP:
-                    self.direction = "UP"
-                    self.angle = 270
-                    self.speed[0] = 0
-                    self.speed[1] = -2
-                elif event.key == pygame.K_s or event.key == pygame.K_DOWN:
-                    self.direction = "DOWN"
-                    self.angle = 90
-                    self.speed[0] = 0
-                    self.speed[1] = 2
-                elif event.key == pygame.K_a or event.key == pygame.K_LEFT:
-                    self.direction = "LEFT"
-                    self.angle = 0
-                    self.speed[0] = -2
-                    self.speed[1] = 0
-                elif event.key == pygame.K_d or event.key == pygame.K_RIGHT:
-                    self.direction = "RIGHT"
-                    self.angle = 180
-                    self.speed[0] = 2
-                    self.speed[1] = 0
+    def get_direction(self, event) -> None:
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_w or event.key == pygame.K_UP:
+                self.direction = "UP"
+                self.angle = 270
+                self.speed[0] = 0
+                self.speed[1] = -2
+            elif event.key == pygame.K_s or event.key == pygame.K_DOWN:
+                self.direction = "DOWN"
+                self.angle = 90
+                self.speed[0] = 0
+                self.speed[1] = 2
+            elif event.key == pygame.K_a or event.key == pygame.K_LEFT:
+                self.direction = "LEFT"
+                self.angle = 0
+                self.speed[0] = -2
+                self.speed[1] = 0
+            elif event.key == pygame.K_d or event.key == pygame.K_RIGHT:
+                self.direction = "RIGHT"
+                self.angle = 180
+                self.speed[0] = 2
+                self.speed[1] = 0
 
-    def rotate_image(self):
+    def rotate_image(self) -> None:
         self.rotated_image = self.image_copy
         if self.direction == "RIGHT":
             self.rotated_image = pygame.transform.flip(self.rotated_image, True, False)
         else:
-           self.rotated_image = pygame.transform.rotate(self.rotated_image, self.angle)
-           self.rect = self.rotated_image.get_rect(center=self.rect.center)
+            self.rotated_image = pygame.transform.rotate(self.rotated_image, self.angle)
+            self.rect = self.rotated_image.get_rect(center=self.rect.center)
         self.image = self.rotated_image
 
-    def move_to_direction(self):
+    def move_to_direction(self) -> None:
         self.rotate_image()
         self.rect.x += self.speed[0]
         self.rect.y += self.speed[1]
 
-    def all_process(self) -> None:
-        self.get_direction()
+    def all_process(self, event) -> None:
+        self.get_direction(event)
         self.move_to_direction()
