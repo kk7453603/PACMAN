067b906ce6d4027958ac187f531a2c0c02ba78af
kirillkom <kk7453603@gmail.com>
2020-11-28 19:53:41 +0300

    get_route function updated v1.1

--- objects/blue_ghost.py
+++ objects/blue_ghost.py
@@ -76,15 +76,25 @@ class Blue_ghost(GhostBase):
     def get_route(self, pacman, blinky, mapp):
         vector = [0, 0]
         # 2 точки после пакмана
-        if mapp[pacman.x + 2] != 0:
+        if mapp[pacman.x + 2][pacman.y] != 0:
             new_x = pacman.x + 2
             new_y = pacman.y
         else:
             new_x = pacman.x
             new_y = pacman.y + 2
         # 2*вектор блики
-        vector[0]=2*(new_x-blinky.x)
-        vector[1]=2*(new_y-blinky.y)
-        return vector
-
-
+        vector[0] = 2 * (new_x - blinky.x)
+        vector[1] = 2 * (new_y - blinky.y)
+        # нахождение клетки с проходом
+        if mapp[vector[0]][vector[1]]!=0:
+            return vector
+        else:
+            if mapp[vector[0]+1][vector[1]]==1:
+                vector[0]+=1
+            elif mapp[vector[0]-1][vector[1]]==1:
+                vector[0]-=1
+            elif mapp[vector[0]][vector[1]+1]==1:
+                vector[1]+=1
+            elif mapp[vector[0]][vector[1]-1]==1:
+                vector[1]-=1
+            return vector
