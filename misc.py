81a28f3b48e3f7cddc79bbfbacdf4cbf1deadc27
Andrew Smirnov <smirnov@informatics.ru>
2020-12-06 21:46:20 +0300

    Ghost class refactor. Add type hints

--- misc.py
+++ misc.py
@@ -1,5 +1,14 @@
+import os
 from random import randint
 
 
 def get_nonzero_random_value(max_abs_value: int) -> int:
     return randint(1, max_abs_value) if randint(0, 1) else randint(-max_abs_value, -1)
+
+
+def get_image_path(*filepath: str) -> str:
+    return os.path.join('images', *filepath)
+
+
+def get_ghost_path(*filepath: str) -> str:
+    return get_image_path('ghost', *filepath)
--- objects/ghost.py
+++ objects/ghost.py
@@ -1,36 +1,33 @@
+from typing import List
+
 import pygame
 
+from misc import get_ghost_path
 from .character import CharacterObject
 
 
 class GhostBase(CharacterObject):
-    up_filename = 'images/ghost/blue/up1.png'
-    down_filename = 'images/ghost/blue/down1.png'
-    left_filename = 'images/ghost/blue/left1.png'
-    right_filename = 'images/ghost/blue/right1.png'
-    scared_filename = 'images/ghost/crazy_ghost/1.png'
-    filename = down_filename
+    filenames: List[str] = [
+        get_ghost_path('blue', 'up1.png'),
+        get_ghost_path('blue', 'down1.png'),
+        get_ghost_path('blue', 'left1.png'),
+        get_ghost_path('blue', 'right1.png'),
+        get_ghost_path('crazy_ghost', '1.png')
+    ]
+    filename: str = filenames[1]
 
-    filenames = [up_filename, down_filename, left_filename, right_filename, scared_filename]
-
-    up_img = pygame.image.load(filenames[0])
-    down_img = pygame.image.load(filenames[1])
-    left_img = pygame.image.load(filenames[2])
-    right_img = pygame.image.load(filenames[3])
-    scared_img = pygame.image.load(filenames[4])
-
-    up_img_resized = pygame.transform.scale(up_img, (35, 35))
-    down_img_resized = pygame.transform.scale(down_img, (35, 35))
-    left_img_resized = pygame.transform.scale(left_img, (35, 35))
-    right_img_resized = pygame.transform.scale(right_img, (35, 35))
-    scared_img_resized = pygame.transform.scale(scared_img, (35, 35))
+    up_img_resized: pygame.Surface = pygame.transform.scale(pygame.image.load(filenames[0]), (35, 35))
+    down_img_resized: pygame.Surface = pygame.transform.scale(pygame.image.load(filenames[1]), (35, 35))
+    left_img_resized: pygame.Surface = pygame.transform.scale(pygame.image.load(filenames[2]), (35, 35))
+    right_img_resized: pygame.Surface = pygame.transform.scale(pygame.image.load(filenames[3]), (35, 35))
+    scared_img_resized: pygame.Surface = pygame.transform.scale(pygame.image.load(filenames[4]), (35, 35))
 
     def __init__(self, game) -> None:
         super().__init__(game)
         self.image = self.up_img_resized
         self.status = 'normal'
 
-    def test_move(self, status) -> None:
+    def test_move(self) -> None:
         pygame.event.pump()
         keys = pygame.key.get_pressed()
 
@@ -54,14 +51,7 @@ class GhostBase(CharacterObject):
     def process_logic(self) -> None:
         pygame.event.pump()
         keys = pygame.key.get_pressed()
-        self.test_move(self.status)
-
-        # При нажатии X приведение переходит в состояние испуга
-        if keys[pygame.K_x]:
-            self.status = 'scared'
-        else:
-            self.status = 'normal'
-
+        self.test_move()
+        self.status = 'scared' if keys[pygame.K_x] else 'normal'
         if self.status == 'scared':
             self.image = self.scared_img_resized
-
