2bbc308f98e62350ad913f9a89a59d563a853385
Mishkina Daria <dasha.mishkina117@gmail.com>
2020-12-12 01:26:26 +0300

    Mishkina: add and fix orange ghost algorithm

--- objects/orange_ghost.py
+++ objects/orange_ghost.py
@@ -16,3 +16,18 @@ class OrangeGhostObject(GhostBase):
     down_img_resized: pygame.Surface = pygame.transform.scale(pygame.image.load(filenames[1]), (17, 17))
     left_img_resized: pygame.Surface = pygame.transform.scale(pygame.image.load(filenames[2]), (17, 17))
     right_img_resized: pygame.Surface = pygame.transform.scale(pygame.image.load(filenames[3]), (17, 17))
+
+    def process_logic(self) -> None:
+        self.define_direction()
+        x1 = self.rect.x
+        y1 = self.rect.y
+        point_i = self.game.scenes[self.game.MAIN_SCENE_INDEX].pacman.pos_on_field[1]
+        point_j = self.game.scenes[self.game.MAIN_SCENE_INDEX].pacman.pos_on_field[0]
+        raw_distance = ((point_j - x1) ** 2 + (point_i - y1) ** 2) ** 0.5
+        distance = raw_distance / 17
+        if distance > 8:
+            # Red Ghost Behavior
+            self.move(self.status, point_i, point_j)
+        else:
+            # Left Down Corner
+            self.move(self.status, 29.5 * 17, 1.5 * 17)
